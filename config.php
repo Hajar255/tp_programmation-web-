c6ef62ca894e868ca1293e39ab5e071a7807ae27
Hajar EL Azzaoui <hajarelazzaoui08@gmail.com>
2025-05-06 17:05:05 +0100

    TP_9

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=inscription_db", "root", "");
+
+// Vérifie si le formulaire est soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = htmlspecialchars($_POST["nom"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    $stmt = $pdo->prepare("INSERT INTO utilisateurs (nom, email, mot_de_passe) VALUES (?, ?, ?)");
+    $stmt->execute([$nom, $email, $password]);
+
+    echo "Inscription réussie !";
+}
+?>
